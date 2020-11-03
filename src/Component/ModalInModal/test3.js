2e753a3776fcc30a277704a6cc7851cb343f8e6d
Menka sha <sha.veroni@gmail.com>
2020-11-03 07:33:06 +0530

    test3,4js added

---
+++ src/Component/ModalInModal/test3.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function test3() {
+  return <div>test3.js</div>;
+}
---
+++ src/Component/ModalInModal/test4.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function test4() {
+  return <div>test 4.js</div>;
+}
